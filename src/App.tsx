add5f69c3378c8fda5b627994112a7f9de1f9ecc
garon <21.gubacsi.aron.mate@mechwart.com>
2025-10-17 11:19:37 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import FAQ from "./components/FAQ.tsx";
+import Background from "./components/Background.tsx";
+
 const App = () => {
   return (
     <main>
+        <Background/>
+        <FAQ/>
     </main>
   )
 }
